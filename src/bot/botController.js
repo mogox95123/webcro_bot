c468947dcd8abb1499a515e05dd36ab50c8b9c9e
mogox95123 <w.gharbi.tangerine@gmail.com>
2024-02-12 18:23:20 -0500

    adding the last needed

--- src/bot/botController.js
+++ src/bot/botController.js
@@ -2,33 +2,44 @@ const TelegramBot = require('node-telegram-bot-api');
 const { addUser, getUserById } = require('../db/models/userModel');
 const { addSubscription } = require('../db/models/subscriptionModel');
 const { addBotLog } = require('../db/models/botLogModel');
-const { botToken } = require('../config/index') // Ensure you have set your bot token in your environment variables
+const { botToken } = require('../config/index')
+const { getUserSubscriptionStatus } = require('./botService')
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(botToken, { polling: true });
 
 // Command listener for "/start"
 bot.on('message', async (msg) => {
+    if(msg != '/subscribe'){
     const chatId = msg.chat.id;
     const username = msg.chat.username;
-    // Try to retrieve the user from the database
     let user = await getUserById(chatId);
     if (!user) {
-        // If user does not exist, add them to the database
         user = await addUser({
             userId: chatId,
             username: username,
             creationDate: new Date(),
-            emailRender: '', // Assume these fields will be set later
+            emailRender: '', 
             passwordRender: '',
             apiKeys: ''
         });
         bot.sendMessage(chatId, "You need a valid key. press /subscribe");
     } else {
-        bot.sendMessage(chatId, "You need a valid key. press /subscribe");
+        const res = await getUserSubscriptionStatus(user.userId)
+        if(res.success){
+            if(res.active){
+                bot.sendMessage(chatId, `Details: ${res.details}\nPress /dashboard`);
+            } else {
+                bot.sendMessage(chatId, "You need a valid key. press /subscribe");
+            }
+        } else {
+            await addBotLog(chatId, '[ERROR 101]', `Username: ${username}`);
+        }
+        
     }
-    // Log the action
+    
     await addBotLog(chatId, 'User started the bot', `Username: ${username}`);
+}
 });
 
 // Command listener for "/subscribe"
